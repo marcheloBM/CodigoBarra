a61c9cfc86f7913171a12b5f39cf9b464b3d381e
Marcelo Burgos <marchelo.bm@gmail.com>
2025-10-25 05:35:13 -0300

    v1.4.2Cambios
    cambios para el APK

--- CodigoBarra/app/src/androidTest/java/com/example/marchelo/myapplication/ApplicationTest.java
+++ CodigoBarra/app/src/androidTest/java/com/example/marchelo/myapplication/ApplicationTest.java
@@ -1,13 +1,21 @@
 package com.example.marchelo.myapplication;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.content.Context;
+import static org.junit.Assert.*;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = ApplicationProvider.getApplicationContext();
+        assertEquals("com.example.marchelo.myapplication", appContext.getPackageName());
     }
 }
